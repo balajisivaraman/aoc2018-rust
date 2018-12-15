35dac6057a7c02b2d7d06f284fcd9275298f5e23
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-12-15 14:46:28 +0530

    Optimize day01b to cycle iterator of nums instead of strings

--- src/year2018.rs
+++ src/year2018.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+
 pub fn day01a(input: Vec<String>) -> i32 {
     input.iter()
         .map(|s| s.parse::<i32>().unwrap())
@@ -7,9 +8,8 @@ pub fn day01a(input: Vec<String>) -> i32 {
 
 pub fn day01b(input: Vec<String>) -> i32 {
     let mut seen_numbers = HashSet::new();
-    let mut acc: i32 = 0;
-    for s in input.into_iter().cycle() {
-        let num = s.parse::<i32>().unwrap();
+    let mut acc = 0;
+    for num in input.into_iter().map(|s| s.parse::<i32>().unwrap()).cycle() {
         acc += num;
         if seen_numbers.contains(&acc) {
             return acc;
